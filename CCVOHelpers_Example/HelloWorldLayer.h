9d1e5a1fe3447337672a9f78cebf73ee2cf58dc6
Alex Damhuis <alex@damhuis.de>
2013-02-24 14:27:04 +0100

    Initial Commit

---
+++ CCVOHelpers_Example/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  CCVOHelpers_Example
+//
+//  Created by Alexander Damhuis on 24.02.13.
+//  Copyright iPhonedation 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ CCVOHelpers_Example/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  CCVOHelpers_Example
+//
+//  Created by Alexander Damhuis on 24.02.13.
+//  Copyright iPhonedation 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
